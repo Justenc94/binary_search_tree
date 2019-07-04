29d9b58874477d6072f05f3578d6de42b597932b
justen <justenc94@gmail.com>
2019-07-03 22:45:29 -0500

    *addNode complete* tested with debug prints that are now deleted.

--- bintree.cpp
+++ bintree.cpp
@@ -32,39 +32,25 @@ bool BinTree::addNode(int id, string info){
 
 //**********************************************   PRIVATE METHODS  **********************************************
 
-//TODO: need to make sure that its accessing a pointer of root because method is never entering recursion
-
 bool BinTree::addNode(int id, string info, DataNode *add_node) {
 
-    if(!add_node->left){
-        cout << "\nLeft is null." << endl;
-    }else{
-        cout << "\nLeft is NOT null." << endl;
-    }
-
-    if(!add_node->right){
-        cout << "Right is null." << endl;
-    }else{
-        cout << "Right is NOT null." << endl;
-    }
-
     if(id < add_node->data.id){
-        cout << "LEFT" << endl;
         if(!add_node->left){
             auto *temp_node = new DataNode;
             temp_node->data.id = id;
             temp_node->data.information = info;
+            temp_node->left = temp_node->right = nullptr;
             add_node->left = temp_node;
             count++;
         }else{
             addNode(id, info, add_node->left);
         }
     }else{
-        cout << "RIGHT" << endl;
         if(!add_node->right){
             auto *temp_node = new DataNode;
             temp_node->data.id = id;
             temp_node->data.information = info;
+            temp_node->left = temp_node->right = nullptr;
             add_node->right = temp_node;
             count++;
         }else{
